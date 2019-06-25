4c6ce93547c64703a417a819789c30442942cb73
wangcong15 <wangcong15@mails.tsinghua.edu.cn>
2019-06-26 01:02:03 +0800

    assertion-insertion

--- checker.go
+++ checker.go
@@ -1,7 +1,67 @@
 package main
 
-func C777(raw_code string) {
-	var result []assertion
-	result = append(result, assertion{"a",1,"b",2})
-	checker_chan <- result
+// CWE-777: Regular Expression without Anchors
+func C777(raw_code string, file_path string) (result assertionSlice) {
+	// demo: result = append(result, assertion{file_path, 2, "123", 777})
+	return
+}
+
+// CWE-478: Missing Default Case in Switch Statement
+func C478(raw_code string, file_path string) (result assertionSlice) {
+	return
+}
+
+// CWE-839: Numeric Range Comparison Without Minimum Check
+func C839(raw_code string, file_path string) (result assertionSlice) {
+	return
+}
+
+// CWE-486: Comparison of Classes by Name
+func C486(raw_code string, file_path string) (result assertionSlice) {
+	return
+}
+
+// CWE-1077: Floating Point Comparison with Incorrect Operator
+func C1077(raw_code string, file_path string) (result assertionSlice) {
+	return
+}
+
+// CWE-785: Use of Path Manipulation Function without Maximum-sized Buffer
+func C785(raw_code string, file_path string) (result assertionSlice) {
+	return
+}
+
+// CWE-466: Return of Pointer Value Outside of Expected Range
+func C466(raw_code string, file_path string) (result assertionSlice) {
+	return
+}
+
+// CWE-822: Untrusted Pointer Dereference
+func C822(raw_code string, file_path string) (result assertionSlice) {
+	return
+}
+
+// CWE-823: Use of Out-of-range Pointer Offset
+func C823(raw_code string, file_path string) (result assertionSlice) {
+	return
+}
+
+// CWE-824: Access of Uninitialized Pointer
+func C824(raw_code string, file_path string) (result assertionSlice) {
+	return
+}
+
+// CWE-128: Wrap-around Error
+func C128(raw_code string, file_path string) (result assertionSlice) {
+	return
+}
+
+// CWE-190: Integer Overflow or Wraparound
+func C190(raw_code string, file_path string) (result assertionSlice) {
+	return
+}
+
+// CWE-191: Integer Underflow (Wrap or Wraparound)
+func C191(raw_code string, file_path string) (result assertionSlice) {
+	return
 }
--- main.go
+++ main.go
@@ -2,84 +2,122 @@ package main
 
 import (
 	"flag"
+	"io/ioutil"
 	"log"
 	"os"
-	"io/ioutil"
-	"strings"
 	"path"
+	"sort"
+	"strings"
 )
 
 var (
-	p string
-	rec_chan chan []assertion
-	checker_chan chan []assertion
+	p        string
+	rec_chan chan assertionSlice
 )
-
-type assertion struct {
-	file_path string
-	line_no int
-	expression string
-	weakness_type int
-}
 
 func init() {
 	flag.StringVar(&p, "p", "", "set a golang package to recommend assertions")
-	rec_chan = make(chan []assertion)
-	checker_chan = make(chan []assertion)
+	rec_chan = make(chan assertionSlice)
 }
 
 func main() {
-	var asserts []assertion
+	var asserts assertionSlice
 	var chan_counter int
 	var file_path string
 	// Flag Parse
 	flag.Parse()
 	if p == "" {
-		log.Println("Package path should not be empty. Use -p to set.")
+		log.Println("==> Package path should not be empty. Use -p to set.")
 		return
 	}
-	log.Printf("Package path is set: %v\n", p)
+	log.Printf("==> Package path is set: %v\n", p)
 
 	// Read File List
 	if s, err := os.Stat(p); err != nil || !s.IsDir() {
-		log.Println("Package path does not exist")
+		log.Println("==> Package path does not exist")
 		return
 	}
 	files, _ := ioutil.ReadDir(p)
 	chan_counter = 0
+	log.Println("==> Source file list: ")
 	for _, f := range files {
-        if strings.HasSuffix(f.Name(), ".go") && !f.IsDir() {
-        	file_path = path.Join(p, f.Name())
-        	chan_counter += 1
-        	// handle source files in concurrent mode
-        	go rec(file_path)
-        }
+		if strings.HasSuffix(f.Name(), ".go") && !f.IsDir() {
+			file_path = path.Join(p, f.Name())
+			chan_counter += 1
+			// handle source files in concurrent mode
+			go rec(file_path)
+		}
 	}
 	for i := 0; i < chan_counter; i++ {
-		val, ok := <- rec_chan
+		val, ok := <-rec_chan
 		if ok {
 			asserts = append(asserts, val...)
 		}
 	}
-	log.Println(asserts)
+	sort.Sort(asserts)
+	log.Println("==> Assertion list: ")
+	for j := range asserts {
+		log.Println(asserts[j])
+	}
+	insert(asserts)
 }
 
+// assertion recommender
 func rec(file_path string) {
-	var result []assertion
+	var result assertionSlice
 	var raw_code string
 
 	log.Println(file_path)
-	raw_code = file_path
+	raw_code = ""
 
 	// checkers in concurrent mode
-	go C777(raw_code)
-
-	for i := 0; i < 1; i++ {
-		val, ok := <- checker_chan
-		if ok {
-			result = append(result, val...)
-		}
-	}
+	result = append(result, C777(raw_code, file_path)...)
+	result = append(result, C478(raw_code, file_path)...)
+	result = append(result, C839(raw_code, file_path)...)
+	result = append(result, C486(raw_code, file_path)...)
+	result = append(result, C1077(raw_code, file_path)...)
+	result = append(result, C785(raw_code, file_path)...)
+	result = append(result, C466(raw_code, file_path)...)
+	result = append(result, C822(raw_code, file_path)...)
+	result = append(result, C823(raw_code, file_path)...)
+	result = append(result, C824(raw_code, file_path)...)
+	result = append(result, C128(raw_code, file_path)...)
+	result = append(result, C190(raw_code, file_path)...)
+	result = append(result, C191(raw_code, file_path)...)
 
 	rec_chan <- result
+}
+
+// Golang does not support transformation from ast to source code.
+// Thus we insert the assertion with file R/W.
+func insert(asserts assertionSlice) {
+	for _, val := range asserts {
+		if b, err := ioutil.ReadFile(val.file_path); err == nil {
+			raw_code := string(b)
+			code_arr := strings.Split(raw_code, "\n")
+			if !strings.Contains(raw_code, "goassert") {
+				for i := range code_arr {
+					if strings.HasPrefix(code_arr[i], "package ") {
+						code_arr[i] += "\nimport \"github.com/wangcong15/goassert\""
+					}
+				}
+			}
+			code_arr[val.line_no-2] += "\n"
+			for j := 0; ; j++ {
+				if j >= len(code_arr[val.line_no]) {
+					break
+				}
+				if code_arr[val.line_no][j] == '\t' {
+					code_arr[val.line_no-2] += "\t"
+				} else {
+					break
+				}
+			}
+			code_arr[val.line_no-2] += val.expression
+			new_code := strings.Join(code_arr, "\n")
+			if ioutil.WriteFile(val.file_path, []byte(new_code), 0644) != nil {
+				log.Printf("==> Error in writing %v\n", val.file_path)
+			}
+		}
+	}
 }
