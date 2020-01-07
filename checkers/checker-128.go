e0ee7bec37fa51c25bbc21721a44461a2998b64d
wangcong15 <wangcong15@mails.tsinghua.edu.cn>
2020-01-07 15:08:11 +0800

    update 128

--- checkers/checker-128.go
+++ checkers/checker-128.go
@@ -9,8 +9,8 @@ import (
 type localVars128 struct {
 	x1      *ast.FuncDecl
 	x2      *ast.AssignStmt
-	x3      []ast.Expr
-	x4      *ast.Ident
+	x3      string
+	x4      *ast.CallExpr
 	x5      []ast.Expr
 	x6      *ast.CallExpr
 	x7      *ast.Ident
@@ -46,72 +46,40 @@ func check128x1(c *code.Code, x1 *ast.FuncDecl, lv *localVars128) {
 
 func check128x2(c *code.Code, x2 *ast.AssignStmt, lv *localVars128) {
 	lv.x2 = x2
-	x3 := x2.Lhs
-	check128x3(c, x3, lv)
-	x5 := x2.Rhs
-	check128x5(c, x5, lv)
-}
-
-func check128x3(c *code.Code, x3 []ast.Expr, lv *localVars128) {
-	lv.x3 = x3
-	for _, x4 := range x3 {
-		if _, ok := x4.(*ast.Ident); ok {
-			check128x4(c, x4.(*ast.Ident), lv)
-		} else {
-			lv.valList = append(lv.valList, "")
-		}
+	x3 := x2.Lhs[0]
+	if _, ok := x3.(*ast.Ident); !ok {
+		return
+	}
+	lv.x3 = x3.(*ast.Ident).Name
+	x4 := x2.Rhs[0]
+	if _, ok := x4.(*ast.CallExpr); ok {
+		check128x4(c, x4.(*ast.CallExpr), lv)
 	}
 }
 
-func check128x4(c *code.Code, x4 *ast.Ident, lv *localVars128) {
+func check128x4(c *code.Code, x4 *ast.CallExpr, lv *localVars128) {
 	lv.x4 = x4
-	lv.valList = append(lv.valList, x4.Name)
-}
-
-func check128x5(c *code.Code, x5 []ast.Expr, lv *localVars128) {
-	lv.x5 = x5
-	for idx, x6 := range x5 {
-		lv.idx = idx
-		if _, ok := x6.(*ast.CallExpr); ok {
-			check128x6(c, x6.(*ast.CallExpr), lv)
+	if len(x4.Args) > 0 {
+		x5 := x4.Fun
+		if _, ok := x5.(*ast.Ident); !ok {
+			return
 		}
-	}
-}
-
-func check128x6(c *code.Code, x6 *ast.CallExpr, lv *localVars128) {
-	lv.x6 = x6
-	if len(x6.Args) > 0 {
-		x7 := x6.Fun
-		if _, ok := x7.(*ast.Ident); ok {
-			check128x7(c, x7.(*ast.Ident), lv)
+		if isWrapFunc(x5.(*ast.Ident).Name) {
+			x6 := x4.Args[0]
+			if _, ok := x6.(*ast.Ident); ok {
+				lv.params = x6.(*ast.Ident).Name + ", " + lv.x3
+				lv.lineNo = c.Fset.Position(lv.x2.TokPos).Line + 1
+				genAssert128(c, lv)
+			}
 		}
-	}
-}
-
-func check128x7(c *code.Code, x7 *ast.Ident, lv *localVars128) {
-	lv.x7 = x7
-	if isWrapFunc(x7.Name) {
-		x8 := lv.x6.Args[0]
-		if _, ok := x8.(*ast.Ident); ok {
-			check128x8(c, x8.(*ast.Ident), lv)
-		}
-	}
-}
-
-func check128x8(c *code.Code, x8 *ast.Ident, lv *localVars128) {
-	lv.x8 = x8
-	if lv.valList[lv.idx] != "" {
-		genAssert128(c, lv)
 	}
 }
 
 func genAssert128(c *code.Code, lv *localVars128) {
-	lineNo := c.Fset.Position(lv.x2.TokPos).Line + 1
-	params := lv.valList[lv.idx] + ", " + lv.x8.Name
 	newAssert := code.Assertion{
 		FuncName: lv.funcName,
-		Params:   params,
-		LineNo:   lineNo,
+		Params:   lv.params,
+		LineNo:   lv.lineNo,
 		BugType:  lv.bugType,
 	}
 	c.Asserts = append(c.Asserts, newAssert)
